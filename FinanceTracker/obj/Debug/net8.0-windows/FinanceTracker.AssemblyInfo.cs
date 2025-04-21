2808517f45a7fcba380b7bb9dd12f469e7777617
SUSEK228 <148262852+SUSEK228@users.noreply.github.com>
2025-04-21 11:34:39 +0200

    aktualizacja

--- FinanceTracker/obj/Debug/net8.0-windows/FinanceTracker.AssemblyInfo.cs
+++ FinanceTracker/obj/Debug/net8.0-windows/FinanceTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac3962b2eed04d096935a0a1e6f1a11874a1310d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a37cfe42952fa549584670626764cb5247972750")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
